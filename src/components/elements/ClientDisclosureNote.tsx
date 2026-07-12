750b6940562d431b22c168ba35fc3c5f9f8688c2
Andrea Malone <114711933+theandreamalone@users.noreply.github.com>
2026-07-11 23:42:23 -0400

    Add files via upload

---
+++ src/components/elements/ClientDisclosureNote.tsx
@@ -0,0 +1,7 @@
+export default function ClientDisclosureNote() {
+  return (
+    <p className="fs-8 text-600 mt-3 mb-0">
+      Client name and additional project detail available on request.
+    </p>
+  );
+}
